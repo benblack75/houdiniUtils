9e8e342fc1e8c3c1da68c3abf21b94e681f21d91
benblack75 <benblack75@mac.com>
2024-02-29 21:58:12 -0800

    Finalized script

--- upload_to_google.py
+++ upload_to_google.py
@@ -1,14 +1,17 @@
 import hou
 
-# Path to my Google Drive Desktop location
-path_to_drive = 'G:/My Drive/HoudiniClass'
-output_path = f'{path_to_drive}/box1.obj'
 
-# Houdini Box node path
-parent_node = hou.pwd()
-target_node = parent_node.node('box1')
 
 def export_to_drive():
-    target_node.geometry().saveToFile(output_path)
+    # Path to my Google Drive Desktop location
+    path_to_drive = 'G:/My Drive/HoudiniClass'
+
+    # Output path
+    output_path = f'{path_to_drive}/box1.obj'
+
+    # Create cube and export
+    geo_node = hou.node('/obj').createNode('geo', 'MyGeometry', run_init_scripts=False)
+    box_node = geo_node.createNode('box')
+    box_node.geometry().saveToFile(output_path)
 
 export_to_drive()
